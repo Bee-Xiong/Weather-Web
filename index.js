8583c00ac8358fbcd42b0c9d75fb19f220147103
Bee Xiong <bxiongent@yahoo.com>
2024-10-02 18:19:18 -0500

    Added hourly weather and weather news

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const btn1 = document.querySelector("#btn1");
 const subMenu = document.querySelector(".sub-menu")
 const toggle = document.querySelector(".toggle");
 const toggleIcon = document.querySelector(".toggle-icon");
-
+const day = document.querySelector(".forecast-daily");
+const hourly = document.querySelector(".forecast-hourly");
 
 
 const dropDown = (event)=> {
@@ -17,12 +18,15 @@ const forecastToggle = ()=> {
     if (toggleIcon.classList.contains("right")) {
         toggleIcon.classList.add("left");
         toggleIcon.classList.remove("right");
+        hourly.classList.remove("active");
+        day.classList.add("active");
     }else {
         toggleIcon.classList.add("right");
         toggleIcon.classList.remove("left");
+        day.classList.remove("active");
+        hourly.classList.add("active");
     }
     console.log(toggle);
-    
 }
 
 btn1.addEventListener("mouseover", dropDown);
